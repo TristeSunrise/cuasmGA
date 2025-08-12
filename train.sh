413a30958d77e1029b04220f37a3179640974333
XinLi <s2626851@ed.ac.uk>
2025-08-12 22:05:34 +0100

    modify train.sh

--- train.sh
+++ train.sh
@@ -3,9 +3,9 @@
 rm -rf data/
 mkdir data/
 
-python3 benchmarks/01-mmleakyrelu.py
-python3 benchmarks/02-batch_matmul.py
-python3 benchmarks/03-fused_feedforward.py
-python3 benchmarks/04-fused-softmax.py
-python3 benchmarks/05-rmsnorm.py
-python3 benchmarks/06-fused-attention.py
+python3 01-mmleakyrelu.py
+python3 02-batch_matmul.py
+python3 03-fused_feedforward.py
+python3 04-fused-softmax.py
+python3 05-rmsnorm.py
+python3 06-fused-attention.py
